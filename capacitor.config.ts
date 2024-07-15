41e6006ec0965b68c00abd6d7f51a9bc1e635fd9
João Silva <61765922+CarDioLogic@users.noreply.github.com>
2024-07-15 17:06:33 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jpmsIonic',
+  webDir: 'www'
+};
+
+export default config;
